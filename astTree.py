55fd35d1e323575e53e0c8df7addd1d5207ac10f
Anders Ardo <anders.ardo@gmail.com>
2025-10-06 21:24:51 +0200

    fixes

--- astTree.py
+++ astTree.py
@@ -7,8 +7,10 @@ from collections import defaultdict
 
 # Globala variabler
 uid = 0
-defined = {}  #name2Id
+defined = {}  #name2Id  BYT NAMN
+localDefined = {} #defined in this file
 defFunk = set()  #id of defined functions (Funk)
+localDefFunk = set()  #id of functions defined in this file
 
 class Node:
     def __init__(self, name, typ, data=''):
@@ -76,13 +78,15 @@ class Node:
             uid += 1
             self.id = uid
             defined[f"{self.name}"] = uid
+        localDefined[f"{self.name}"] = self.id
         for n in self.nodes:
             n.assignIds()
 
-    def definedFunctions(self):
+    def definedFunctions(self):  #BYT NAMN
         global defFunk
         if self.typ == 'Funk':
             defFunk.add(self.id)
+            localDefFunk.add(self.id)
         for n in self.nodes:
             n.definedFunctions()
 
@@ -225,76 +229,44 @@ try:
 except Exception as e:
     sys.exit(f"{projDir} är inte en katalog {e}")
 
-fileData = {}
+allData = {}  #all data per file
+fileData = {}  #ast per file
 # Gå igenom alla PHP-filer i projdDir recursivt utom 'smarty' ??
-for f in Path(projDir).rglob('*.php'):
-    if '/smarty/' in str(f):
-        #print(f"SKIP {f}")
-        continue
+for filPath in Path(projDir).rglob('*.php'):
+    f = str(filPath)
+    if '/smarty/' in f: continue
+    if 'Web/Dis' in f: continue
     try:
-        if not 'MSPDO1' in str(f): continue  #DEB
         print(f"Analyserar '{f}'")
-        SPARA ALLA resultat per fil i fileData - ast, defined, id2Namn, defFunk, allFuncs, externa_anrop, inteAnropadeNamn ...
+        allData[f] = {'ast': None, 'defined': {}, 'id2Namn': {}, 'defFunk': set(), 'calls': set(), 'anropade': set(),
+                      'allFuncs': set(), 'externa_anrop': set(), 'inteAnropadeNamn': set(), 'graphDot': ''}
+        localDefined = {}
+        localDefFunk = set()
         fileData[f] = None
         fileAST = ast(f)
         fileData[f] = fileAST.getTree()
+        allData[f]['ast'] = fileData[f]
         fileData[f].assignIds()
-        x = fileData[f].definedFunctions()
-        # Rita graph
-        dotfil = 'data/' + os.path.split(f)[1].replace('php', 'dot')
-        graph = fileData[f].mkGraph()
-        with open(f"{dotfil}", 'w') as fil:
-            fil.write(graph)  #graph.to_string())
-            fil.write("}\n")
-        os.system(f"dot -Tsvg {dotfil} > {dotfil.replace('.dot', '.svg')}")
-        if '->' in graph:
-            containsEdges = True
-        else:
-            containsEdges = False
-        id2Name = {v: k for k, v in defined.items()}
+        # data per fil
+        allData[f]['defined'] = localDefined
+        fileData[f].definedFunctions() #funktioner använda i denna fil
+        allData[f]['defFunk'] = localDefFunk #funktioner i definierade i denna fill
+        allData[f]['id2Name'] = {v: k for k, v in localDefined.items()}
+        allData[f]['allFuncs'] = set(allData[f]['defined'].values())  #Alla funktioner i denna fil
+        allData[f]['externa_anrop'] = list(allData[f]['allFuncs'].difference(allData[f]['defFunk']))  #anropade externa funktioner
+        allData[f]['externaAnropadeNamn'] = [ allData[f]['id2Name'][x] for x in  allData[f]['externa_anrop'] ]
+        allData[f]['graphDot'] = allData[f]['ast'].mkGraph() # UTAN externa anrop - för att kunna hitta anropade
         #anropade (called) functions
-        calls = defaultdict(set)
-        anropade = set()
-        for l in graph.splitlines():
+        allData[f]['calls'] = defaultdict(set)
+        allData[f]['anropade'] = set()
+        for l in allData[f]['graphDot'].splitlines():
             try:
                 (caller, callee) = l.strip().rstrip(';').split(' -> ')
-                calls[caller].add(int(callee))
-                anropade.add(int(callee))
+                allData[f]['calls'][caller].add(int(callee))
+                allData[f]['anropade'].add(int(callee))  #Behöver uppdateras med anrop från andra filer
             except:
                 pass
-        # Generera HTML för varje PHP-fil
-        allFuncs = set(defined.values())  #Alla funktioner
-        externa_anrop = list(allFuncs.difference(defFunk))  #anrop till externa funktioner
-        with open(f"{dotfil.replace('.dot', '.html')}", 'w') as html:
-            (dir, filnamn) = os.path.split(f)
-            dir = f"{dir}/".replace(projDir, '')
-            filnamn = filnamn.replace('.php', '')
-            html.write(f"""<!DOCTYPE html>
-            <html>
-            <head><title>Statisk call-graf för {filnamn}</title></head>
-            <body>
-            <h1>Statisk call-graf för {filnamn}</h1>
-            <a href='./top_index.html'>Tillbaka till top index</a>
-            """)
-            if allFuncs.difference(anropade).difference(externa_anrop):
-                html.write("<h2>Inte !lokalt! anropade funktioner</h2>\n")
-                inteAnropadeNamn = [id2Name[x] for x in allFuncs.difference(anropade).difference(externa_anrop)]
-                html.write(', '.join(sorted(inteAnropadeNamn)))
-                html.write("<p>\n")
-            #printa externa_anrop
-            if externa_anrop:
-                html.write("<h2>Externa anrop till:</h2>\n")
-                externaAnropadeNamn = [id2Name[x] for x in externa_anrop]
-                html.write(', '.join(sorted(externaAnropadeNamn)))
-            # FIX KOLLA OM det finns graf!
-            if containsEdges:
-                html.write("<h2>Call-graf</h2>\n")
-                html.write("""Röd? anger anrop från en annan fil.<br>
-                Gul anger "public functions".<br>
-                Grön anger "private functions".<br>\n""")
-                html.write(f"<img src='{dotfil.replace('data/', '').replace('.dot', '.svg')}' width='100%'>\n")
-            html.write("</body>\n</html>\n")
-
+        allData[f]['inteAnropadeNamn'] = [allData[f]['id2Name'][x] for x in  allData[f]['allFuncs'].difference(allData[f]['anropade']).difference(allData[f]['externa_anrop'])]
     except Exception as e:
         import traceback
         print(traceback.print_exception(e))
@@ -307,20 +279,92 @@ for f in Path(projDir).rglob('*.php'):
             log.write(f"MISSLYCKADES med {f}\n")
             log.write(str(type(e)) + "\n")
             log.write("".join(traceback.format_exception(e)))
-            
-sys.exit()
 
-#find duplicate defined functions
-print("Test dubbletter")
+id2Name = {v: k for k, v in defined.items()}
 
-x = set()
-id2Name = {}
-for f,id in defined.items():
-    if '../Disbyt/' in f: continue
-    if f in x:
-        print(f"Dubblett id={id} namn={f}")
-    x.add(f)
-    id2Name[id] = f
+##Generera resultatfiler
+
+for f,data in allData.items():
+    #if not 'MSPDO1' in str(f): continue  #DEB
+    # Generera HTML för varje PHP-fil
+    allFuncs = data['allFuncs']  #set(defined.values())  #Alla funktioner
+    externa_anrop = data['externa_anrop']  #list(allFuncs.difference(defFunk))  #anrop till externa funktioner
+    dotfil = './data/' + os.path.split(f)[1].replace('php', 'dot')
+    #Graph med externa anrop
+    allaAnrop = set()
+    for fx,fxData in allData.items():
+        if fxData['anropade']:
+            allaAnrop.update(set(fxData['anropade']))
+    lokalaFunkInteAnropade = (allData[f]['defFunk'] & allaAnrop) -  allData[f]['anropade']
+    lokalaFunkExterntAnropade = allData[f]['defFunk'] & allaAnrop
+    if data['graphDot']:
+        dotAdd = 'Extern  [fillcolor="coral", style=filled, shape="box"];\n'
+        for id in lokalaFunkExterntAnropade:
+            dotAdd += f"Extern -> {id};\n"
+        graph = allData[f]['graphDot'] + dotAdd
+        with open(f"{dotfil}", 'w') as fil:
+            fil.write(graph)  #graph.to_string())
+            fil.write("}\n")
+        os.system(f"dot -Tsvg {dotfil} > {dotfil.replace('.dot', '.svg')}")
+    with open(f"{dotfil.replace('.dot', '.html')}", 'w') as html:
+        (dir, filnamn) = os.path.split(f)
+        dir = f"{dir}/".replace(projDir, '')
+        filnamn = filnamn.replace('.php', '')
+        html.write(f"""<!DOCTYPE html>
+        <html>
+        <head><title>Statisk call-graf för {filnamn}</title></head>
+        <body>
+        <h1>Statisk call-graf för {filnamn}</h1>
+        <a href='./top_index.html'>Tillbaka till top index</a>
+        """)
+        """??
+        if allFuncs.difference(anropade).difference(externa_anrop):
+            html.write("<h2>Inte !lokalt! anropade funktioner ?</h2>\n")
+            inteAnropadeNamn = data['inteAnropadeNamn']  #[id2Name[x] for x in allFuncs.difference(anropade).difference(externa_anrop)]
+            html.write(', '.join(sorted(inteAnropadeNamn)))
+            html.write("<p>\n")
+        """
+        #printa externa_anrop
+        if externa_anrop:
+            html.write("<h2>Externa anrop till: ?</h2>\n")
+            externaAnropadeNamn = data['externaAnropadeNamn']
+            html.write(', '.join(sorted(externaAnropadeNamn)))
+        # FIX KOLLA OM det finns graf!
+        if ' -> ' in graph:
+            html.write("<h2>Call-graf</h2>\n")
+            html.write("""Röd? anger anrop från en annan fil.<br>
+            Gul anger "public functions".<br>
+            Grön anger "private functions".<br>\n""")
+            html.write(f"<img src='{dotfil.replace('data/', '').replace('.dot', '.svg')}' width='100%'>\n")
+        html.write("</body>\n</html>\n")
+
+#Global analys
+
+## Kolla dubbletter och inte anropade
+#print('Funktioner definierade i mer än 1 fil')
+dubl = defaultdict(int)
+for f, data in allData.items():
+    for id in allData[f]['defFunk']:
+        if id2Name[id] == '__construct': continue
+        dubl[id] += 1
+dublDef = {}
+for id,ant in dubl.items():
+    if ant > 1:
+        files = []
+        for f, data in allData.items():
+            if id in data['defFunk']:
+                files.append(f)
+        #print(f"{id}, {id2Name[id]}, {','.join(files)}")
+        dublDef[id2Name[id]] = files
+#
+#inte anropade - alla allaDefFunk - allaAnropade
+allaAnropade = set()
+for f,data in allData.items(): #?
+    allaAnropade.update(set(data['anropade']))
+allaInteAnropade = defFunk - allaAnropade
+#print(allaInteAnropade)
+#for id in allaInteAnropade:
+#    print(id, id2Name[id])
 
 with open('./data/top_index.html', 'w') as html:
     html.write(f"""<!DOCTYPE html>
@@ -345,9 +389,23 @@ with open('./data/top_index.html', 'w') as html:
             html.write(f"<td>-</td></tr>\n")
     html.write("</table>\n")
     #Inte anropade
-    #  totNotCalled
     html.write("<h2>Inte använda funktioner</h2>\n")
-    #?? html.write(', '.join(sorted(list(totNotCalled))))
-    #Externa anrop
-    # Gör graf av totExt vilka filer som anropar andra filer
-    #?? html.write("<h2>Projekt call-graf (filer)</h2>\n")
+    for id in allaInteAnropade:
+        html.write(f"{id2Name[id]}, ")
+    #html.write(f"{allaInteAnropade}")
+    html.write("<h2>Funktioner definierade mer än 1 gång</h2>\n")
+    html.write("<table border=1>\n")
+    html.write("<tr><th>Funktion</th><th>File</th></tr>\n")
+    for namn, filer in dublDef.items():
+        html.write(f"<tr><td>{namn}</td><td>{'<br>'.join(filer)}</td></tr>\n")
+    html.write("</table>\n")
+    #html.write(f"{dublDef}")
+    html.write("</body></html>")
+"""
+#Check inte använda
+    for id in allaInteAnropade:
+        namn = id2Name[id]
+        cmd = f'grep -r -e "{namn}" {projDir}'
+        ext = subprocess.check_output(cmd, shell=True)
+        print(ext)
+"""
