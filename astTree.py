9ee5f654ebf49ba15798bd370bea20f477d33724
Anders Ardo <anders.ardo@gmail.com>
2025-10-07 21:57:56 +0200

    Projkt graph

--- astTree.py
+++ astTree.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import re
 import subprocess
 from pathlib import Path
 import json
 from collections import defaultdict
+import traceback
 
 # Globala variabler
 uid = 0
@@ -12,9 +14,17 @@ localDefined = {} #defined in this file
 defFunk = set()  #id of defined functions (Funk)
 localDefFunk = set()  #id of functions defined in this file
 
+try:
+    projDir = sys.argv[1]
+    print(f"Projektkatalog={projDir}")
+    if not os.path.isdir(projDir):
+        help(f"'{projDir}' är inte en katalog")
+except Exception as e:
+    sys.exit(f"{projDir} är inte en katalog {e}")
+
 class Node:
     def __init__(self, name, typ, data=''):
-        self.name = name
+        self.name = name.replace(projDir, '')  #Remove start-path 
         self.id = '?'
         if typ in ('Fil', 'Klass', 'Funk', 'Anrop'):
             self.typ = typ
@@ -37,7 +47,7 @@ class Node:
     def mkGraph(self, graph=None, id=None, prefix=''):
         #Graphviz colors: cornflowerblue, deepskyblue, red, chartreuse, gold, coral, green, yellow
         if not graph:
-            self.graph = "digraph testmk {nodesep=0.3; overlap=False; rankdir=LR; ranksep=0.25; concentrate=true;\n"
+            self.graph = '' #"digraph testmk {nodesep=0.3; overlap=False; rankdir=LR; ranksep=0.25; \n"
             graph = self.graph
             done = set()
         if self.typ in ('Fil', 'Klass', 'Funk'):
@@ -46,10 +56,10 @@ class Node:
             label = f"{self.name}"  #Minimal
             color = 'firebrick1'
             if self.typ == 'Fil':
-                graph += f'{self.id} [fillcolor="chartreuse", label="{label}", style=filled, shape="box"];\n'                
+                graph += f'{self.id} [fillcolor="cornflowerblue", label="{label}", style=filled, shape="box"];\n'                
             elif self.typ == 'Klass':
                 color = 'coral'
-                graph += f'subgraph cluster{self.name} {{graph [style=rounded] label="Klass {label}"\n'
+                graph += f'subgraph cluster{self.name} {{graph [style=rounded]  label="KLASS: {label}"\n'
             elif self.typ == 'Funk' and self.data == 'public':
                 graph += f'{self.id} [fillcolor="gold", label="{label}", style=filled, shape="box"];\n'
             elif (self.typ == 'Funk') and (self.data == 'private'):
@@ -152,9 +162,9 @@ class ast():
             for key,val in ast.items():
                 self.process(val)
         except Exception as e:
-            print(' ERR: doMethodCall' , e)
-            print('  ', ast)
-            sys.exit()
+            #print(' ERR: doMethodCall' , e)
+            #print('  ', ast)
+            pass
 
     def doAST_Call(self, ast):
     #    {'AST_CALL':
@@ -220,22 +230,20 @@ class ast():
         self.process(self.astData)
         #print(self.varMap)
         return self.tree
-    
-try:
-    projDir = sys.argv[1]
-    print(f"Projektkatalog={projDir}")
-    if not os.path.isdir(projDir):
-        help(f"'{projDir}' är inte en katalog")
-except Exception as e:
-    sys.exit(f"{projDir} är inte en katalog {e}")
 
 allData = {}  #all data per file
 fileData = {}  #ast per file
-# Gå igenom alla PHP-filer i projdDir recursivt utom 'smarty' ??
+# Gå igenom alla PHP-filer i projdDir recursivt utom
+skipDir = ['/smarty/', '/Disuppd/', '/Disconv/', '/DisgenDubblett/'] #
 for filPath in Path(projDir).rglob('*.php'):
     f = str(filPath)
-    if '/smarty/' in f: continue
-    if 'Web/Dis' in f: continue
+    p = '|'.join(skipDir)
+    pattern = re.compile(r'%s' % p)
+    m = re.search(pattern, f)
+    if m: continue
+    #if '/smarty/' in f: continue
+    #if 'Web/Disconv/' in f: continue
+    #? REMOVE all resultfiles
     try:
         print(f"Analyserar '{f}'")
         allData[f] = {'ast': None, 'defined': {}, 'id2Namn': {}, 'defFunk': set(), 'calls': set(), 'anropade': set(),
@@ -256,6 +264,17 @@ for filPath in Path(projDir).rglob('*.php'):
         allData[f]['externa_anrop'] = list(allData[f]['allFuncs'].difference(allData[f]['defFunk']))  #anropade externa funktioner
         allData[f]['externaAnropadeNamn'] = [ allData[f]['id2Name'][x] for x in  allData[f]['externa_anrop'] ]
         allData[f]['graphDot'] = allData[f]['ast'].mkGraph() # UTAN externa anrop - för att kunna hitta anropade
+        #compress graph - remove duplicate links
+        compressedGraph = ''
+        doneLink = set()
+        for line in allData[f]['graphDot'].splitlines():
+            line = line.strip()
+            if ' -> ' in line and line in doneLink:
+                continue
+            else:
+                compressedGraph += line + "\n"
+                doneLink.add(line)
+        allData[f]['graphDot'] = compressedGraph
         #anropade (called) functions
         allData[f]['calls'] = defaultdict(set)
         allData[f]['anropade'] = set()
@@ -268,12 +287,11 @@ for filPath in Path(projDir).rglob('*.php'):
                 pass
         allData[f]['inteAnropadeNamn'] = [allData[f]['id2Name'][x] for x in  allData[f]['allFuncs'].difference(allData[f]['anropade']).difference(allData[f]['externa_anrop'])]
     except Exception as e:
-        import traceback
-        print(traceback.print_exception(e))
-        print(f"MISSLYCKADES med {f}\n{e}\n")
-        print(type(e))
-        print(e.args)
-        print(traceback.format_exc())
+        #print(traceback.print_exception(e))
+        print(f"MISSLYCKADES med {f} se logfil\n")
+        #print(type(e))
+        #print(e.args)
+        #print(traceback.format_exc())
         logfil = 'data/' + os.path.split(f)[1].replace('php', 'log')
         with open(logfil, 'w') as log:
             log.write(f"MISSLYCKADES med {f}\n")
@@ -303,6 +321,7 @@ for f,data in allData.items():
             dotAdd += f"Extern -> {id};\n"
         graph = allData[f]['graphDot'] + dotAdd
         with open(f"{dotfil}", 'w') as fil:
+            fil.write("digraph testmk {nodesep=0.3; overlap=False; rankdir=LR; ranksep=0.25; \n")
             fil.write(graph)  #graph.to_string())
             fil.write("}\n")
         os.system(f"dot -Tsvg {dotfil} > {dotfil.replace('.dot', '.svg')}")
@@ -326,15 +345,17 @@ for f,data in allData.items():
         """
         #printa externa_anrop
         if externa_anrop:
-            html.write("<h2>Externa anrop till: ?</h2>\n")
+            html.write("<h2>Externa anrop till:</h2>\n")
             externaAnropadeNamn = data['externaAnropadeNamn']
             html.write(', '.join(sorted(externaAnropadeNamn)))
         # FIX KOLLA OM det finns graf!
         if ' -> ' in graph:
             html.write("<h2>Call-graf</h2>\n")
-            html.write("""Röd? anger anrop från en annan fil.<br>
+            html.write("""Blå är den analyserade filen.<br>
+            Röd anger anrop från en annan fil.<br>
             Gul anger "public functions".<br>
-            Grön anger "private functions".<br>\n""")
+            Grön anger "private functions".<br>
+            Vit är okända funktioner - möjligen från någon av de filer som inte har analyserats.<br>\n""")
             html.write(f"<img src='{dotfil.replace('data/', '').replace('.dot', '.svg')}' width='100%'>\n")
         html.write("</body>\n</html>\n")
 
@@ -353,7 +374,7 @@ for id,ant in dubl.items():
         files = []
         for f, data in allData.items():
             if id in data['defFunk']:
-                files.append(f)
+                files.append(f.replace(projDir, './'))
         #print(f"{id}, {id2Name[id]}, {','.join(files)}")
         dublDef[id2Name[id]] = files
 #
@@ -362,15 +383,31 @@ allaAnropade = set()
 for f,data in allData.items(): #?
     allaAnropade.update(set(data['anropade']))
 allaInteAnropade = defFunk - allaAnropade
-#print(allaInteAnropade)
-#for id in allaInteAnropade:
-#    print(id, id2Name[id])
 
+#Projekt graf
+#Fixa graph i 'mkGraph' - lägg till vid utskrift
+#lägg till 'graphDot' för alla filer
+graph = ''
+k = 1
+for f,data in allData.items():
+    graph += f'subgraph cluster{k} {{graph [style=rounded]  label="FIL: {f}"\n'
+    graph += data['graphDot']
+    graph += "}\n"
+    k += 1
+pgrfil = './data/projektGraph.dot'
+with open(pgrfil, 'w') as fil:
+    fil.write("digraph Projekt {nodesep=0.3; overlap=False; rankdir=LR; ranksep=0.25; \n")
+    fil.write(graph)
+    fil.write("}\n")
+os.system(f"dot -Tsvg {pgrfil} > {pgrfil.replace('.dot', '.svg')}")
 with open('./data/top_index.html', 'w') as html:
     html.write(f"""<!DOCTYPE html>
     <html>
-    <head><title>Statiska call-grafer för {projDir}</title></head>
+    <head><title>Statiska call-grafer för projektet {projDir}</title></head>
     <body>
+    <a href="totLog.txt">Log för hela körningen</a><br>
+    <a href="projektGraph.svg">Graph över hela projeket (STOR)</a>
+    <h4>Skippade kataloger: {', '.join(skipDir)}</h4>
     <h1>Statiska call-grafer för {projDir}</h1>
     """)
     html.write("<table border=1>\n")
@@ -379,10 +416,10 @@ with open('./data/top_index.html', 'w') as html:
         (dir, filnamn) = os.path.split(fil)
         dir = f"{dir}/".replace(projDir, '')
         filnamn = filnamn.replace('.php', '')
-        if Path(f"./data/{filnamn}.html").is_file():
+        if Path(f"./data/{filnamn}.html").is_file() and not Path(f"./data/{filnamn}.log").is_file():
             html.write(f"<tr><td>./{dir}</td><td><a href='{filnamn}.html'>{filnamn}</a></td>")
         else:
-            html.write(f"<tr><td>./{dir}</td><td>ERR {filnamn}</td>")
+            html.write(f"<tr><td>./{dir}</td><td>{filnamn} <b>?</b></td>")
         if Path(f"./data/{filnamn}.log").is_file():
             html.write(f"<td><a href='{filnamn}.log'>Log</a></td></tr>\n")
         else:
@@ -395,7 +432,7 @@ with open('./data/top_index.html', 'w') as html:
     #html.write(f"{allaInteAnropade}")
     html.write("<h2>Funktioner definierade mer än 1 gång</h2>\n")
     html.write("<table border=1>\n")
-    html.write("<tr><th>Funktion</th><th>File</th></tr>\n")
+    html.write("<tr><th>Funktion</th><th>Filer</th></tr>\n")
     for namn, filer in dublDef.items():
         html.write(f"<tr><td>{namn}</td><td>{'<br>'.join(filer)}</td></tr>\n")
     html.write("</table>\n")
